ad8438f79a02c9dbca268912d1c77ddc198212d1
pureconnor <conye.productions@gmail.com>
2021-09-27 11:47:35 -0400

    add link to repo

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,19 +10,21 @@ export const Header = memo(function Header({
   const baseClasses =
     'flex justify-end h-header box-content px-12 py-4 bg-white'
   return (
-    <div
-      className={`${baseClasses} ${className}`}
-      style={{ height: 60 }}
-    >
-      <div className="h-16 w-16 rounded-full shadow-md bg-gray-300">
-        <Image
-          className="block w-auto h-auto max-w-full max-h-full"
-          src={'/me.webp'}
-          alt="Profile Icon"
-          height="100%"
-          width="100%"
-        />
+    <a href="https://github.com/pureconnor/org-explorer">
+      <div
+        className={`${baseClasses} ${className}`}
+        style={{ height: 60 }}
+      >
+        <div className="h-16 w-16 rounded-full shadow-md bg-gray-300">
+          <Image
+            className="block w-auto h-auto max-w-full max-h-full"
+            src={'/me.webp'}
+            alt="Profile Icon"
+            height="100%"
+            width="100%"
+          />
+        </div>
       </div>
-    </div>
+    </a>
   )
 })
